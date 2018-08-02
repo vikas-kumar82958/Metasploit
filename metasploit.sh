f77cf8aa02c5c1c02558288c9c4ceffb1fd5968e
vikas-kumar82958 <vk312233@gmail.com>
2018-08-02 22:37:08 +0530

    This is a Hacking Environmental

---
+++ metasploit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+termux-setup-storage
+apt-get update && apt-get upgrade -y
+pkg install curl clang git wget python2 -y
+pkg install python php fish bash ruby perl nmap proot w3m -y 
+wget https://Auxilus.github.io/metasploit.sh
+chmod +x metasploit.sh
+./metasploit.sh
