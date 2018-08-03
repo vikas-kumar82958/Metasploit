a011cbebaa120dc7969c12b53de82cca0ebbef44
vikas-kumar82958 <41354331+vikas-kumar82958@users.noreply.github.com>
2018-08-03 10:04:36 +0530

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 termux-setup-storage
 apt-get update && apt-get upgrade -y
-pkg install curl clang git wget python2 -y
+pkg install curl clang git wget python2 nano -y
 pkg install python php fish bash ruby perl nmap proot w3m -y 
 wget https://Auxilus.github.io/metasploit.sh
 chmod +x metasploit.sh
