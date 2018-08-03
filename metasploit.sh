6cb6d731c7e378b13c72ac4091816e3342c5943d
vikas-kumar82958 <41354331+vikas-kumar82958@users.noreply.github.com>
2018-08-03 10:03:16 +0530

    Delete metasploit.sh

--- metasploit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-termux-setup-storage
-apt-get update && apt-get upgrade -y
-pkg install curl clang git wget python2 -y
-pkg install python php fish bash ruby perl nmap proot w3m -y 
-wget https://Auxilus.github.io/metasploit.sh
-chmod +x metasploit.sh
-bash metasploit.sh
