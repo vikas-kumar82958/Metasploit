ed14d15dab14d355fe011e481f4b2446b69e4438
Vikas kumar <41354331+vikas-kumar82958@users.noreply.github.com>
2018-08-11 20:02:04 +0530

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -2,8 +2,17 @@
 termux-setup-storage
 apt-get update && apt-get upgrade -y
 pkg install curl clang git wget python2 nano -y
-pkg install python php fish bash ruby perl nmap proot w3m -y 
+pkg install python php fish bash ruby perl nmap proot w3m -y
+pkg install db postgresql parallel -y
 wget https://Auxilus.github.io/metasploit.sh
 chmod +x metasploit.sh
 bash metasploit.sh
+ls
 cd $HOME
+mkdir -p $PREFIX/var/lib/postgresql
+initdb $PREFIX/var/lib/postgresql
+pg_ctl -D $PREFIX/var/lib/postgresql start
+cd metasploit-framework
+ls
+./msfconsole
+exit 0
